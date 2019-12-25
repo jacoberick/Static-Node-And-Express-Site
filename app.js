7978a66bec89a96411b3f182d7972d92c0341b30
Jacob White <jacobewhitedev@gmail.com>
2019-12-25 03:11:57 -0600

    added projects to index and project photos to /projects:id

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // required dependencies
 const express = require("express");
 const data = require("./data");
-
+const { projects } = data.data;
 const app = express();
 
 // sets view engine to pug ▼・ᴥ・▼
@@ -12,7 +12,7 @@ app.use("/static", express.static("public"));
 
 // renders the home page
 app.get("/", (req, res) => {
-  res.render("index", { data: data.projects });
+  res.render("index", { projects });
 });
 
 //renders about page
@@ -20,14 +20,12 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
-app.get("/project/:id", (req, res, next) => {
-  res.render("project", {
-    id: parseInt(req.params.id),
-    data: data.projects
-  });
+app.get("/project/:id", (req, res) => {
+  let project = projects.filter(p => +p.id === +req.params.id)[0];
+  res.render("project", { project });
 });
 
-// lisetens to port 3000 and logs a success messaage
+// listens to port 3000 and logs a success message
 app.listen(3000, () => {
-  console.log("The app is running on port 3000. sick!");
+  console.log("The app is running on port 3000! sick.");
 });
